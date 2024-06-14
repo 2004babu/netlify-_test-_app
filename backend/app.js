529562e875a682ff888fbe1e2e80793a00a378c7
babu20041509 <bb5696359@gmail.com>
2024-06-14 19:20:08 +0530

    small test content

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const cors = require('cors');
+
+// Load environment variables from .env file
+dotenv.config({ path: path.join(__dirname, '.env') });
+
+const app = express();
+
+// Use CORS middleware
+app.use(cors());
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message:process.env.NAME
+    });
+});
+
+process.on('uncaughtException', (err) => {
+    console.log(err);
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
